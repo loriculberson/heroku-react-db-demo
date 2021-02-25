0860c4702dc04f4588f5c33d42f4b62fff9e841c
Lori Culberson <bigskybandit@sbcglobal.net>
2021-02-25 01:28:43 -0500

    Create build directory

---
+++ client/build/precache-manifest.7249511c2eb3d7e139570c9497a87ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1d2fff12d6d416836929eacf38d94b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1169d27fa4eef11b6ecf",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "7a3f91e66fe81af00a08",
+    "url": "/static/js/2.b35bf4f7.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.b35bf4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1169d27fa4eef11b6ecf",
+    "url": "/static/js/main.36411c91.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
