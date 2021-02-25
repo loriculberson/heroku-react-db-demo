acee4e7561f0813628542d037b31feef088e77cd
Lori Culberson <bigskybandit@sbcglobal.net>
2021-02-25 01:23:58 -0500

    Deployment config added to seedDB

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -4,8 +4,13 @@ const db = require("../models");
 // This file empties the Books collection and inserts the books below
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
 
 const bookSeed = [
